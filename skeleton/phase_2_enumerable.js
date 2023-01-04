6898fb74aafa366b410a0c72ad459faa0d3ae1a1
Kevin Liu <kliu33@buffalo.edu>
2023-01-04 17:30:44 -0500

    finished part 2

--- skeleton/phase_2_enumerable.js
+++ skeleton/phase_2_enumerable.js
@@ -1,15 +1,26 @@
-Array.prototype.myEach = function(callback) {
-    for (i=0; i < this.length; i++) {
-        this[i] = callback(this[i]);
-    }
-}
+Array.prototype.myEach = function (callback) {
+  for (i = 0; i < this.length; i++) {
+    callback(this[i]);
+  }
+  // return this
+};
 
-Array.prototype.myMap = function(callback) {
-    const mapped = [];
-    return this.myEach(function(callback) {
-        for (i=0; i < this.length; i++) {
-            return mapped.push(callback(this[i]));
-        }
-    });
-   
+Array.prototype.myMap = function (callback) {
+  const mapped = [];
+  // this.myEach(function(el) {
+  //     mapped.push(callback(el));
+  // });
+  const cb = function (el) {
+    mapped.push(callback(el));
+  };
+  this.myEach(cb);
+  return mapped;
+};
+
+Array.prototype.myReduce = function (callback, initialValue) {
+  acc = initialValue || this.shift();
+  this.myEach(function (el) {
+    acc = callback(acc, el);
+  });
+  return acc;
 };
